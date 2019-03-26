6cf8f1d456d4327c8538746bca6e44e10f28d99f
IPonomarchuk <47009382+IPonomarchuk@users.noreply.github.com>
2019-03-26 11:34:30 +0200

    done, package is renamed.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import Tasks.ArrayCheck;
-import Tasks.Palindrome;
-import Tasks.Pie;
+import tasks.ArrayCheck;
+import tasks.Palindrome;
+import tasks.Pie;
 
 public class Main {
 
@@ -9,11 +9,9 @@ public class Main {
         Palindrome.isPalindrome("Madam, I’m Adam");
 
         int[] array = {1, 0, 5, 13, 100, 3};
-
         ArrayCheck.sumOfArrayElements(array);
 
         Pie.biggestPieceOfPie();
 
     }
-
 }
